84ced760587b82127831dd7b38fc1d9a61672026
Robert <robertjohnconcepcion@gmail.com>
2017-05-19 00:13:07 +0800

    initilize witty map settings control

---
+++ admin/partials/witty-map-option-page.php
@@ -0,0 +1,80 @@
+<div class="wrap">
+    <h1><?php echo _x('Witty Map Settings', 'witty_map' ); ?></h1>
+
+    <form method="post" action="options.php">
+
+        <?php 
+        /**
+         * witty_map_backend
+         *
+         * @hooked option_page_before_form
+         */
+        do_action( "witty_map_after_form" );
+
+        $support = new witty_support;
+
+        ?>
+        <table class="form-table">
+
+            <tr valign="top">
+                <th scope="row">
+                    <label for="googlemapapi-key"><?php echo _x('Google map api key','witty_map'); ?></label>
+                </th>
+                <td>
+                    <?php 
+                        $support->witty_template( 'inc', 'witty-field-text', [ 
+                            'name'  => 'googlemapapi_key', 
+                            'value' => $googlemap_api, 
+                            'attrb' => [
+                                'id'    =>  'googlemapapi-key',
+                                'class' =>  'regular-text'
+                            ],
+                        ] );
+                    ?>
+                </td>
+            </tr>
+        
+            <tr valign="top">
+                <th scope="row">
+                    <label for="wittymap-center"><?php echo _x('Map Center','witty_map'); ?></label>
+                </th>
+                <td>
+                    <?php 
+                        $support->witty_template( 'inc', 'witty-field-text', [ 
+                            'name'  => 'wittymap_loc', 
+                            'value' => $wittymap_loc, 
+                            'attrb' => [
+                                'id'    =>  'wittymap-center',
+                                'class' =>  'regular-text'
+                            ],
+                        ] );
+                    ?>
+                </td>
+            </tr>
+
+            <tr valign="top">
+                <th scope="row">
+                    <label for="wittymap-center"><?php echo _x('Default Map Zoom','witty_map'); ?></label>
+                </th>
+                <td>
+                    <?php 
+                        $support->witty_template( 'inc', 'witty-field-num', [ 
+                            'name'  => 'wittymap_def_zoom', 
+                            'value' => $wittymap_def_zoom, 
+                            'attrb' => [
+                                'id'    =>  'wittymap-center',
+                                'class' =>  'regular-text'
+                            ],
+                        ] );
+                    ?>
+                </td>
+            </tr>
+
+        </table>
+
+
+        
+        <?php submit_button(); ?>
+
+    </form>
+</div>
