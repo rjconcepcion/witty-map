f416e6fbbc8b35f5115a6a7e72262ac9274fa68a
Robert <robertjohnconcepcion@gmail.com>
2017-05-19 00:14:12 +0800

    init dir for basic partial helpers

---
+++ inc/partials/witty-field-num.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The numberfield of witty
+ * 
+ * @author Robert John Concepcion
+ */
+?>
+<input type="number" name="<?php echo $name; ?>" value="<?php echo $value; ?>" <?php echo witty_support::attrb( $attrb ); ?> />
---
+++ inc/partials/witty-field-text.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * The textfield of witty plugin
+ * 
+ * @author Robert John Concepcion
+ */
+?>
+<input type="text" name="<?php echo $name; ?>" value="<?php echo $value; ?>" <?php echo witty_support::attrb( $attrb ); ?> />
