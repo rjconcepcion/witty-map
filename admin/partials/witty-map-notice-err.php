ceb682e2c40fb8b30bfe20479581c2690e49f52f
Robert <robertjohnconcepcion@gmail.com>
2017-05-21 22:47:49 +0800

    init admin notice message

---
+++ admin/partials/witty-map-notice-err.php
@@ -0,0 +1,12 @@
+<?php 
+/**
+ * Witty map error show if user does not set
+ * google map api key.
+ *
+ * @version 1
+ * @author Robert John Concepcion
+ */
+?>
+<div class="notice notice-error">
+    <p><b>Witty Map</b> needs your help!! please configure the <a href='https://developers.google.com/maps/documentation/javascript/get-api-key' target='_blank'>google map api key</a> in the <a href="admin.php?page=witty-map-settings">Witty Map</a> page :-(</p>
+</div>
